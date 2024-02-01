2d9e85572f8642055252f7fd1a882ca940a67a75
PetrovicNeda <nedapetrovic@outlook.com>
2024-02-01 21:41:01 +0100

    Added new tests

--- src/test/java/Tests/RegistrationTest.java
+++ src/test/java/Tests/RegistrationTest.java
@@ -68,6 +68,38 @@ public class RegistrationTest extends BaseTest {
         Assert.assertEquals(loginPage.errorSignupMessage.getText(), "Email Address already exist!");
         Assert.assertEquals(driver.getCurrentUrl(), "https://automationexercise.com/signup");
     }
+    @Test(priority = 30)
+    public void userCannotSingupWithEmptyMandatoryField(){
+        String name = "korisnik";
+        String email = "korisnik2@mail.com";
+        loginPage.inputNameForSignup(name);
+        loginPage.inputEmailForSignup(email);
+        loginPage.clickOnSignupButton();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://automationexercise.com/signup");
+
+        String password = "korisnik123";
+//        String firstName = "Petar";
+        String lastName = "Petrovic";
+        String address = "Address 1";
+        String state = "State";
+        String city = "City";
+        String zipcode = "123000";
+        String mobile = "0632568455";
+        registrationPage.inputPassword(password);
+        scrollToElement(registrationPage.firstNameField);
+//        registrationPage.inputFirstName(firstName);
+        registrationPage.inputLastName(lastName);
+        registrationPage.inputAddress(address);
+        registrationPage.clickOnCountryDropDownMenu("Australia");
+        registrationPage.inputState(state);
+        registrationPage.inputCity(city);
+        registrationPage.inputZipcode(zipcode);
+        registrationPage.inputMobile(mobile);
+        registrationPage.clickOnCreateAccButton();
+
+        Assert.assertTrue(registrationPage.createAccButton.isDisplayed());
+        Assert.assertEquals(driver.getCurrentUrl(), "https://automationexercise.com/signup");
+    }
 }
 
 
