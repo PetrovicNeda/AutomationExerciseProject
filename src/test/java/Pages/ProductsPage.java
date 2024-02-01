63a9223fe8238560f6365d5b49b793faae17b06d
PetrovicNeda <nedapetrovic@outlook.com>
2024-02-01 21:42:14 +0100

    Added new WebElements and methods

--- src/test/java/Pages/ProductsPage.java
+++ src/test/java/Pages/ProductsPage.java
@@ -1,10 +1,15 @@
 package Pages;
 
 import BaseTest.BaseTest;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import java.time.Duration;
 import java.util.List;
 
 public class ProductsPage extends BaseTest {
@@ -12,6 +17,8 @@ public class ProductsPage extends BaseTest {
     public ProductsPage() {
         PageFactory.initElements(driver, this);
     }
+
+    CartPage cartPage = new CartPage();
 
     @FindBy(css = "a[href='/brand_products/Polo'")
     public WebElement poloButton;
@@ -23,6 +30,10 @@ public class ProductsPage extends BaseTest {
     public WebElement viewCartButton;
     @FindBy(css = "a[href='/view_cart']")
     public WebElement cartButton;
+    @FindBy(css = ".badge.pull-right")
+    public List<WebElement> category;
+    @FindBy(css = "a[href='/category_products/1'")
+    public WebElement dressCategoryButton;
 
 //-------------------------------------------
 
@@ -40,6 +51,21 @@ public class ProductsPage extends BaseTest {
     public void clickOnCartButton(){
         cartButton.click();
     }
+    public void addItemToCartAndOpetCart(){
+        scrollToElement(poloButton);
+        clickOnAddToCartButton(0);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".btn.btn-success.close-modal.btn-block")));
+        Assert.assertTrue(continueShoppingButton.isDisplayed());
 
+        clickOnViewCartButton();
+        Assert.assertTrue(cartPage.products.get(0).isDisplayed());
+    }
+    public void clickOnCategory(int j){
+            category.get(j).click();
+    }
+    public void clickOnDressCategoryButton(){
+        dressCategoryButton.click();
+    }
 
 }
